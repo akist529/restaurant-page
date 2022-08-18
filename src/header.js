e58d47d99b0e90c72eb4fb882f11e399988f2dd3
Alex Kist <akist0707@gmail.com>
2022-08-17 21:34:07 -0500

    Work on header

--- src/header.js
+++ src/header.js
@@ -2,17 +2,34 @@ export default function headerBar() {
     const container = document.createElement("div");
     container.id = "header";
 
-    const homeButton = document.createElement("button");
-    homeButton.textContent = "Home";
-    container.appendChild(homeButton);
+    let width = 0;
 
-    const menuButton = document.createElement("button");
-    menuButton.textContent = "Menu";
-    container.appendChild(menuButton);
+    for (let i = 0; i < 9; i++) {
+        const fret = document.createElement("div");
+        fret.className = "fret";
 
-    const contactButton = document.createElement("button");
-    contactButton.textContent = "Contact";
-    container.appendChild(contactButton);
+        fret.style.width = `${width}px`;
 
+        if (i === 4) {
+            const homeButton = document.createElement("button");
+            homeButton.textContent = "Home";
+            fret.appendChild(homeButton);
+        } else if (i === 6) {
+            const menuButton = document.createElement("button");
+            menuButton.textContent = "Menu";
+            fret.appendChild(menuButton);
+        } else if (i === 8) {
+            const contactButton = document.createElement("button");
+            contactButton.textContent = "Contact";
+            buttonLayer.appendChild(contactButton);
+        }
+
+        container.appendChild(fret);
+
+        width += (window.innerWidth / 100);
+    }
+
+    container.appendChild(overlay);
+    container.appendChild(buttonLayer);
     document.body.appendChild(container);
 }
