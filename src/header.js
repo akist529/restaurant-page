bff3ab6a36a3e137123378301b6c90b4647eed84
Alex Kist <akist0707@gmail.com>
2022-08-17 19:56:11 -0500

    Add page buttons to header

--- src/header.js
+++ src/header.js
@@ -1,5 +1,18 @@
 export default function headerBar() {
     const container = document.createElement("div");
     container.id = "header";
+
+    const homeButton = document.createElement("button");
+    homeButton.textContent = "Home";
+    container.appendChild(homeButton);
+
+    const menuButton = document.createElement("button");
+    menuButton.textContent = "Menu";
+    container.appendChild(menuButton);
+
+    const contactButton = document.createElement("button");
+    contactButton.textContent = "Contact";
+    container.appendChild(contactButton);
+
     document.body.appendChild(container);
 }
