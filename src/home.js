fde6f8001936e000cd27051ab465e00e3df45b2f
Alex Kist <akist0707@gmail.com>
2022-08-18 21:03:50 -0500

    Work on basic layout of homepage

--- src/home.js
+++ src/home.js
@@ -4,7 +4,29 @@ export default function homePage() {
 
     const homeTitle = document.createElement("h1");
     homeTitle.textContent = "Home";
-
+    homeTitle.className = "contentTitle";
     container.appendChild(homeTitle);
+
+    const review = document.createElement("div");
+    const reviewText = document.createElement("p");
+    reviewText.textContent = "Bla bla bla";
+    review.appendChild(reviewText);
+    review.className = "contentBlock";
+    container.appendChild(review);
+
+    const hours = document.createElement("div");
+    const hoursText = document.createElement("p");
+    hoursText.textContent = "9:00AM - 5:00PM";
+    hours.appendChild(hoursText);
+    hours.className = "contentBlock";
+    container.appendChild(hours);
+
+    const location = document.createElement("div");
+    const locationText = document.createElement("p");
+    locationText.textContent = "1234 Some Street";
+    location.appendChild(locationText);
+    location.className = "contentBlock";
+    container.appendChild(location);
+
     document.getElementById("content").appendChild(container);
 }
