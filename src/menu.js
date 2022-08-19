96d5c0add58c7ee75d54dd917394c5588a8b55fd
Alex Kist <akist0707@gmail.com>
2022-08-19 16:26:24 -0500

    Work on basic layout of menu

--- src/menu.js
+++ src/menu.js
@@ -3,8 +3,48 @@ export default function menuPage() {
     container.id = "menu";
 
     const menuTitle = document.createElement("h1");
-    menuTitle.textContent = "Menu";
-
+    menuTitle.textContent = "Our Menu";
+    menuTitle.className = "contentTitle";
     container.appendChild(menuTitle);
+
+    function menuCat(name) {
+        const category = document.createElement("div");
+        category.id = name.split(" ").join("-").toLowerCase();
+        console.log(category.id);
+        const catTitle = document.createElement("h2");
+        catTitle.textContent = name;
+        category.appendChild(catTitle);
+
+        container.appendChild(category);
+    }
+
+    function menuItem(name, desc, price, category) {
+        const item = document.createElement("div");
+        item.className = "menuItem";
+
+        const itemName = document.createElement("h3");
+        itemName.textContent = name;
+
+        const itemDesc = document.createElement("p");
+        itemDesc.textContent = desc;
+
+        const itemPrice = document.createElement("p");
+        itemPrice.textContent = price;
+
+        item.appendChild(itemName);
+        item.appendChild(itemDesc);
+        item.appendChild(itemPrice);
+
+        const catID = category.split(" ").join("-").toLowerCase();
+        document.getElementById(catID).appendChild(item);
+    }
+
+    menuCat("Starters");
+    menuItem("Starter Item", "Starter Description", "$9.99", "Starters")
+
+    menuCat("Main Show");
+    menuCat("Encore");
+    menuCat("The Bar");
+
     document.getElementById("content").appendChild(container);
 }
